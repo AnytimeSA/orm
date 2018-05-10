3499ecaeaeb1d23396f01a7c9a1ad54261486c04
dve <david.vanderelst@anyti.me>
2018-05-10 16:39:07 +0200

    Add EntityManager::__constructor and EntityManager::loadAndGetRepository methods

--- src/EntityManager/EntityManager.php
+++ src/EntityManager/EntityManager.php
@@ -2,6 +2,42 @@
 
 namespace DVE\EntityORM\EntityManager;
 
-class EntityManager
+abstract class EntityManager
 {
+    /**
+     * @var EntityRepository[]
+     */
+    protected $loadedRepositories;
+
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * EntityManager constructor.
+     * @param \PDO $pdo
+     */
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    /**
+     * @param string $class
+     * @param string $tableName
+     * @return EntityRepository
+     */
+    protected function loadAndGetRepository(string $class, string $tableName)
+    {
+        if(array_key_exists($class, $this->loadedRepositories)) {
+            return $this->loadedRepositories[$class];
+        }
+
+        if(class_exists($class)) {
+            return (new $class())->setTableName($tableName);
+        } else {
+            return (new DefaultEntityRepository())->setTableName($tableName);
+        }
+    }
 }
