d3d7903c61def69fe6b475cd556433eb44a27672
dve <david.vanderelst@anyti.me>
2018-07-08 14:29:11 +0200

    Add tests for class DVE\EntityORM\Tests\Converter\SnakeToCamelCaseStringConverter

---
+++ tests/Converter/SnakeToCamelCaseStringConverterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace DVE\EntityORM\Tests\Converter;
+
+use DVE\EntityORM\Converter\SnakeToCamelCaseStringConverter;
+use PHPUnit\Framework\TestCase;
+
+class SnakeToCamelCaseStringConverterTest extends TestCase
+{
+    /**
+     * @group Converter
+     * @group SnakeToCamelCaseStringConverter
+     * @dataProvider getDataSet
+     * @param string $snakeCaseString
+     * @param string $expectedConvertedString
+     */
+    public function testConvertMethod(string $snakeCaseString, string $expectedConvertedString)
+    {
+        $converter = new SnakeToCamelCaseStringConverter();
+        $this->assertSame($expectedConvertedString, $converter->convert($snakeCaseString));
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getDataSet()
+    {
+        return [
+            ['SOMESTRING', 'SOMESTRING'],
+            ['Some_string', 'SomeString'],
+            ['SomeString', 'SomeString'],
+            ['some_string', 'SomeString'],
+            ['SOME_STRING', 'SOMESTRING'],
+            ['_some_string', 'SomeString'],
+            ['_SOME_STRING_', 'SOMESTRING'],
+            ['_some_string', 'SomeString']
+        ];
+    }
+}
