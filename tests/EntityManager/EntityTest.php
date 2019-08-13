30a3668c86233500593e14986eba5c0faa090786
dve <david.vanderelst@anyti.me>
2019-08-13 12:54:57 +0200

    Create missing Entity unit tests + fix exception message

--- tests/EntityManager/EntityTest.php
+++ tests/EntityManager/EntityTest.php
@@ -17,8 +17,8 @@ class EntityTest extends ORMTestCase
         $entity = $this->getEntity();
         $this->assertSame(
             [
-                'id' => 10,
-                'id2' => 20
+                'id' => 1,
+                'id2' => 2
             ],
             $entity->extractPrimaryKeyValues()
         );
@@ -33,8 +33,8 @@ class EntityTest extends ORMTestCase
         $entity = $this->getEntity();
         $this->assertSame(
             [
-                'id'            => 10,
-                'id2'           => 20,
+                'id'            => 1,
+                'id2'           => 2,
                 'some_field'    => 'Some string'
             ],
             $entity->extractData()
@@ -64,19 +64,120 @@ class EntityTest extends ORMTestCase
     }
 
     /**
-     * @return Entity|__anonymous@501
+     * @group EntityManager
+     * @group Entity
      */
-    private function getEntity()
+    public function testUpdateNeeded()
     {
-        return new class extends Entity {
-            const TABLENAME = 'test_entity';
-            const PRIMARY_KEYS = ['id', 'id2'];
+        $foo = $this->getEntity();
+        $this->assertFalse($foo->updateNeeded(), 'If a setter of an initialized entity has never been called, the method should returns false');
 
-            protected $data = [
-                'id' => 10,
-                'id2' => 20,
-                'some_field' => 'Some string'
-            ];
-        };
+        $foo->setSomeField('test 2');
+        $this->assertTrue($foo->updateNeeded(), 'If a setter of an initialized entity has been called at least one time, the method should returns true');
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function testResetDataSetterUsedGlobally()
+    {
+        $this->assertSame([], $this->getEntity()->setSomeField('new string')->resetDataSetterUsed()->extractSetterUsedData());
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function testResetDataSetterUsedWithOneFieldInParam()
+    {
+        $this->assertSame(['some_field' => 'new string'], $this->getEntity()->setSomeField('new string')->resetDataSetterUsed('id2')->extractSetterUsedData());
+        $this->assertSame([], $this->getEntity()->setSomeField('new string')->resetDataSetterUsed('some_field')->extractSetterUsedData());
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function testGetEntityPropertyTypeOnExistingField()
+    {
+        $this->assertSame('string', Foo::getEntityPropertyType('some_field'));
+        $this->assertSame('int', Foo::getEntityPropertyType('id'));
+        $this->assertSame('int', Foo::getEntityPropertyType('id2'));
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function testGetEntityPropertyTypeOnNonExistingField()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Entity property "non_existing_field" not found for entity ' . Foo::class);
+        Foo::getEntityPropertyType('non_existing_field');
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function testIsPropertyNullableOnExistingField()
+    {
+        $this->assertTrue(Foo::isPropertyNullable('some_field'));
+        $this->assertFalse(Foo::isPropertyNullable('id'));
+        $this->assertFalse(Foo::isPropertyNullable('id2'));
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function testIsPropertyNullableOnNonExistingField()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Entity property "non_existing_field" not found for entity ' . Foo::class);
+        Foo::isPropertyNullable('non_existing_field');
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function getPropertyDefaultValueOnExistingField()
+    {
+        $this->assertSame('default value', Foo::getPropertyDefaultValue('some_field'));
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function getPropertyDefaultValueOnNonExistingField()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Entity property "non_existing_field" not found for entity ' . Foo::class);
+        Foo::getPropertyDefaultValue('non_existing_field');
+    }
+
+    /**
+     * @group EntityManager
+     * @group Entity
+     */
+    public function testIsPropertyExists()
+    {
+        $this->assertTrue(Foo::isPropertyExists('some_field'));
+        $this->assertFalse(Foo::isPropertyExists('non_existing_field'));
+    }
+
+    /**
+     * @return Entity
+     */
+    private function getEntity(): Foo
+    {
+        return new Foo([
+            'id' => 1,
+            'id2' => 2,
+            'some_field' => 'Some string'
+        ]);
     }
 }
--- tests/Stub/Foo.php
+++ tests/Stub/Foo.php
@@ -4,13 +4,14 @@ namespace Anytime\ORM\Tests\Stub;
 
 use Anytime\ORM\EntityManager\Entity;
 
-class Foo extends Entity 
+class Foo extends Entity
 {
     const TABLENAME = 'foo_entity';
-    const PRIMARY_KEYS = ['id'];
+    const PRIMARY_KEYS = ['id', 'id2'];
 
     protected $data = [
-	    'id' => 1,
+	    'id' => 0,
+        'id2' => 0,
 	    'some_field' => ''
     ];
 
@@ -19,9 +20,62 @@ class Foo extends Entity
         'some_field' => false
     ];
 
-    public function setSomeField($value)
+    protected static $sqlFieldStruct = [
+        'id' => array (  'tableName' => 'foo',  'fieldName' => 'id',  'type' => 'int',  'allowNull' => false,  'keyType' => 'PRI',  'defaultValue' => NULL,  'dateFormat' => ''),
+        'id2' => array (  'tableName' => 'foo',  'fieldName' => 'id2',  'type' => 'int',  'allowNull' => false,  'keyType' => 'PRI',  'defaultValue' => NULL,  'dateFormat' => ''),
+        'some_field' => array (  'tableName' => 'foo',  'fieldName' => 'some_field',  'type' => 'string',  'allowNull' => true,  'keyType' => '',  'defaultValue' => 'default value',  'dateFormat' => '')
+    ];
+
+    public function setSomeField($value): Foo
     {
         $this->data['some_field'] = $value;
         $this->dataSetterUsed['some_field'] = true;
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     * @return Foo
+     */
+    public function setId(int $value): Foo
+    {
+        $this->data['id'] = $value;
+        $this->dataSetterUsed['id'] = true;
+        return $this;
+    }
+
+    /**
+     * @param int $value
+     * @return Foo
+     */
+    public function setId2(int $value): Foo
+    {
+        $this->data['id2'] = $value;
+        $this->dataSetterUsed['id2'] = true;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return (int)$this->data['id'];
+    }
+
+    /**
+     * @return int
+     */
+    public function getId2(): int
+    {
+        return (int)$this->data['id2'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getSomeField(): string
+    {
+        return (string)$this->data['some_field'];
     }
 };
